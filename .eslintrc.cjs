8527395e8f2ac5f0f736007a2e76c34e4fe2a5de
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-09-11 22:46:57 +0300

    init: add gulp, structure, fonts

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    // 'import/prefer-default-export': 'off',
+    // 'no-undef': 'off',
+    // 'func-names': 'off',
+    // 'consistent-return': 'off',
+    // 'no-unused-vars': 'off',
+    // 'prefer-destructuring': 'off',
+    // 'no-restricted-globals': 'off',
+    // 'no-param-reassign': 'off',
+    'import/extensions': 'off',
+  },
+};
